a337a4c0725efbbe3a332860d34db9ad241c11d6
BhanuPrakash <prakashelidindi@gmail.com>
2022-04-10 23:20:46 +0530

    Commit Stack implementatiom using LinkedList and ArrayList

--- Scaler/src/main/java/DataStructuresBasics/Stacks_MinStack.java
+++ Scaler/src/main/java/DataStructuresBasics/Stacks_MinStack.java
@@ -43,3 +43,103 @@ public class Stacks_MinStack {
 	}
 
 }
+
+
+//Min Stack Implementation using ArrayList
+//class Solution {
+//  List<Integer> value_stack = new ArrayList<Integer>();
+//  List<Integer> min_stack = new ArrayList<Integer>();
+//  int top = -1;
+//  public void push(int x) {
+//      if(min_stack.size()==0)
+//      {
+//          min_stack.add(x);
+//      }
+//      else
+//      {
+//          int curr_min = min_stack.get(top);
+//          min_stack.add(Math.min(x,curr_min));
+//      }  
+//      value_stack.add(x);
+//      top++; 
+//  }
+
+//  public void pop() {
+//      if(top!=-1){
+//          min_stack.remove(top);
+//          value_stack.remove(top);
+//          top--;
+//      }
+//  }
+//  public int top() {
+//      if(top!=-1)
+//      {
+//          return value_stack.get(top);
+//      }
+//      return -1;
+//  }
+
+//  public int getMin() {
+//       if(top!=-1)
+//      {
+//          return min_stack.get(top);
+//      }
+//      return -1;
+//  }
+//}
+
+
+//Min Stack Implementation using LinkedList
+//class Node{
+// int val;
+// Node next;
+// public Node(int x){
+//     this.val = x;
+// }
+//}
+//
+//class Solution {
+//Node head;
+//Node min_head;
+// public void push(int x) {
+//     if(min_head == null)
+//     {
+//         min_head = new Node(x);
+//         head = new Node(x);
+//     }
+//     else
+//     {
+//         int curr_min = min_head.val;
+//         int sel_min = Math.min(x,curr_min);
+//         Node new_min_node = new Node(sel_min);
+//
+//         new_min_node.next = min_head;
+//         min_head = new_min_node;
+//
+//         Node new_node = new Node(x);
+//         new_node.next=head;
+//         head = new_node;
+//     }
+// }
+//
+// public void pop() {
+//     if(head != null){
+//         head = head.next;
+//         min_head = min_head.next;
+//     }
+// }
+// public int top() {
+//     if(head!=null)
+//          return head.val;
+//         
+//     return -1;
+// }
+//
+// public int getMin() {
+//     if(min_head!=null)
+//         return min_head.val;
+//     
+//     return -1;
+// }
+//}
+
